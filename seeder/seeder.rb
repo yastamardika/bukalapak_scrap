07e9c66a1e49ea3e09557f656f900d202d102dc1
yastamardika <rattyasta1@gmail.com>
2022-03-08 04:23:41 +0700

    add config and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,10 @@
+pages << {
+    page_type: "listings",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://www.bukalapak.com/c/sepeda/fullbike/roadbike",
+    vars: {
+      category: "Roadbike"
+    },
+    fetch_type: "browser"
+}
