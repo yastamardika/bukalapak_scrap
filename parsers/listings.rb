6e17612ec1f2bf55394e5a5dd72ceb1efb22ce0d
yastamardika <rattyasta1@gmail.com>
2022-03-08 13:08:58 +0700

    add vars for max page

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -17,15 +17,24 @@ product_list.each do |product|
     }
 end
 
-pagination_links = html.css('ul.bl-ghostblock-pagination__list li a')
+# max_page = html.css('ul.bl-ghostblock-pagination__list li.has-page-active span[aria-current="true"]')
+# p max_page
+page_num = page["vars"]["page_num"]
+url = "https://www.bukalapak.com/c/sepeda/fullbike/roadbike?page#{next_page}"
+page_num += 1
 # p pagination_links
-pagination_links.each do |link|
-    new_url = link["href"]
-    url = "https://www.bukalapak.com/c/sepeda/fullbike/roadbike#{link["href"]}"
+if page_num <= page["vars"]["max_page"]
     pages << {
         url: url,
         page_type: 'listings',
-        fetch_type: 'browser',
-        vars: {}
+        method: "GET",
+        headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+        vars: {
+            category: "Roadbike",
+            page_num: page_num,
+            max_page: 99
+          },
+        fetch_type: "browser"
         }
 end
+
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,7 +4,9 @@ pages << {
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
     url: "https://www.bukalapak.com/c/sepeda/fullbike/roadbike",
     vars: {
-      category: "Roadbike"
+      category: "Roadbike",
+      page_num: 2,
+      max_page: 99
     },
     fetch_type: "browser"
 }
